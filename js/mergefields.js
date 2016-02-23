57222e9fa98164796c53953c4260e3a9ce118075
tyleryoungblood <tyler@resimplifi.com>
2016-02-23 13:35:50 -0500

    add hidden /copy/index.html page

---
+++ js/mergefields.js
@@ -0,0 +1,200 @@
+var mergefields =
+[ //############################################################################
+  {"label":"header",        "value":"Buyer"},
+  //############################################################################
+
+    {"label":"subheader",                         "value":"Agency Representation"},
+      {"label":"Agreement Commencement Date",     "value":""},
+      {"label":"Agreement Expiration Date",       "value":""},
+      {"label":"Agents Commission (estimate)%",   "value":""},
+      {"label":"Representation Term (months)",    "value":""},
+
+    {"label":"subheader",         "value":"Search Requirements"},
+      {"label":"Square Footage",  "value":"Search Square Footage"},
+      {"label":"Property Type",   "value":"Search Property Type"},
+      {"label":"Market/Subtype",  "value":"Search Market/Submarket"},
+      {"label":"Parking Spaces",  "value":"Search Parking Spaces"},
+      {"label":"Price",           "value":"Search Price"},
+
+    {"label":"subheader",       "value":"Buyer (Primary Contact)"},
+      {"label":"Full Name",     "value":"Primary Buyer Contact: Full Name"},
+      {"label":"First Name",    "value":"Primary Buyer Contact: First Name"},
+      {"label":"Last Name",     "value":"Primary Buyer Contact: Last Name"},
+      {"label":"Title",         "value":"Primary Buyer Contact: Title"},
+      {"label":"Department",    "value":"Primary Buyer Contact: Department"},
+      {"label":"Email",         "value":"Primary Buyer Contact: Email"},
+      {"label":"Fax",           "value":"Primary Buyer Contact: Fax"},
+      {"label":"Phone",         "value":"Primary Buyer Contact: Business Phone"},
+      {"label":"Mobile Phone",  "value":"Primary Buyer Contact: Mobile Phone"},
+
+  //############################################################################
+  {"label":"header",                            "value":"Seller"},
+  //############################################################################
+
+    {"label":"subheader",                       "value":"Agreement Details"},
+      {"label":"Agreement Commencement Date",   "value":""},
+      {"label":"Agreement Expiration Date",     "value":""},
+      {"label":"Listing Term",                  "value":""},
+      {"label":"Gross Listing Commission %",    "value":""},
+      {"label":"Sales Price",                   "value":""},
+
+  //############################################################################
+  {"label":"header",                            "value":"Landlord"},
+  //############################################################################
+
+    {"label":"subheader",                       "value":"Seller (Primary Contact)"},
+      {"label":"Agreement Commencement Date",   "value":"Listing Agreement Commencement Date"},
+      {"label":"Agreement Expiration Date",     "value":"Listing Agreement Expiration Date"},
+      {"label":"Listing Term",                  "value":"Listing Agreement Term Months"},
+      {"label":"Gross Listing Commission %",    "value":"Gross Commission Percentage"},
+      {"label":"Liating Rental Rate $",         "value":"Listing Monthly Square Foot Rate"},
+      {"label":"Projected Rental Increase",     "value":""},
+      {"label":"Projected Term (Years)",        "value":"Term Years"},
+
+  //############################################################################
+  {"label":"header",                                      "value":"Tennant"},
+  //############################################################################
+
+    {"label":"subheader",                                 "value":"Agency Representation"},
+      {"label":"Agreement Commencement Date",             "value":""},
+      {"label":"Agreement Expiration Date",               "value":""},
+      {"label":"Tenant Agents Commission (estimate)%",    "value":"Tenant Agent Commission"},
+      {"label":"Representation Term (months)",            "value":""},
+
+  //############################################################################
+  {"label":"header",                        "value":"Property"},
+  //############################################################################
+
+    {"label":"subheader",                   "value":"Property Information"},
+      {"label":"Property Type",             "value":"Search Property Type"},
+      {"label":"Property Name",             "value":"Property: Property Name"},
+      {"label":"Street Address",            "value":"Property: Address"},
+      {"label":"City",                      "value":"Property: City"},
+      {"label":"State",                     "value":"Property: State"},
+      {"label":"Postal Code",               "value":"Property: Billing Zip/ Postal Code"},
+      {"label":"County",                    "value":"Property: County"},
+      {"label":"Country",                   "value":"Property: Country"},
+      {"label":"Zoning",                    "value":"Property: Zoning"},
+      {"label":"Tax ID Number",             "value":"Property: Tax ID Number"},
+      {"label":"Owners Legal Name",         "value":"Property: Ownership Name (Legal Entity)"},
+      {"label":"Parking Spaces Available",  "value":"Property: Parking Available"},
+      {"label":"Parking Ratio",             "value":"Property: Parking Ratio"},
+      {"label":"Description",               "value":"Property: Description"},
+      {"label":"Total Building Size",       "value":"Total Building Size"},
+      {"label":"Number of Floors",          "value":"Property: Floor"},
+      {"label":"Year Built",                "value":"Property: Year Built"},
+      {"label":"Common Area Factor",        "value":"Property: Common Area Factor"},
+      {"label":"Acreage",     "value":""},
+
+    {"label":"subheader",         "value":"Images"},
+      {"label":"Primary Image",   "value":""},
+      {"label":"Secondary Image", "value":""},
+      {"label":"Tertiary Image",  "value":""},
+
+  //############################################################################
+  {"label":"header",        "value":"Transaction"},
+  //############################################################################
+
+    {"label":"subheader",                                 "value":"Suite Information"},
+      {"label":"Suite Number",                            "value":"Property: Suite Number"},
+      {"label":"Suite Size",                              "value":"Property: Suite Size"},
+      {"label":"Total Building Size",                     "value":"Total Building Size"},
+      {"label":"Pro Rata Share Building Opex",            "value":"Pro Rata Share"},
+      {"label":"Description of Demised Premises/Suite",   "value":"Suite Description"},
+
+    {"label":"subheader",                                 "value":"Earnest Money"},
+      {"label":"Earnest Money",                           "value":"Earnest Money (Sale)"},
+      {"label":"Earnest Money Amount",                    "value":"Earnest Money Amount"},
+      {"label":"Close Date ",                             "value":"Close Date"},
+      {"label":"Due Diligence Period Expiration Date",    "value":"Due Diligence Period Expiration Date"},
+
+    {"label":"subheader",                                 "value":"Commission Details"},
+      {"label":"Gross Value of Transaction $",            "value":"Transaction Size"},
+      {"label":"Gross Commission %",                      "value":"Gross Commission Percentage"},
+      {"label":"Company Commission %",                    "value":"Company Commission Percent"},
+      {"label":"Co-Broker Commission %",                  "value":"Co-Broker Commission Split"},
+      {"label":"Gross Commission $",                      "value":"Gross Commission"},
+      {"label":"Company Commission $",                    "value":"Company Commission Amount"},
+      {"label":"Co-Broker Commission $",                  "value":""},
+      {"label":"Commission to be paid by",                "value":"Commission To Be Paid By"},
+      {"label":"Commission Notes",                        "value":"Commission Notes"},
+
+  //############################################################################
+  {"label":"header",        "value":"Contact"},
+  //############################################################################
+
+    {"label":"subheader",         "value":"Buyer Acount"},
+      {"label":"Account Name",    "value":"Buyer Account: Account Name"},
+      {"label":"Street Address",  "value":"Buyer Account: Billing Street"},
+      {"label":"City",            "value":"Buyer Account: Billing City"},
+      {"label":"State",           "value":"Buyer Account: Billing State/Province"},
+      {"label":"Zipcode",         "value":"Buyer Account: Billing Zip/Postal Code"},
+      {"label":"Country",         "value":"Buyer Account: Country"},
+
+    {"label":"subheader",         "value":"Buyer Contact (Primary)"},
+      {"label":"Full Name",       "value":"Primary Buyer Contact: Full Name"},
+      {"label":"First Name",      "value":"Primary Buyer Contact: First Name"},
+      {"label":"Last Name",       "value":"Primary Buyer Contact: Last Name"},
+      {"label":"Title",           "value":"Primary Buyer Contact: Title"},
+      {"label":"Department",      "value":"Primary Buyer Contact: Department"},
+      {"label":"Email",           "value":"Primary Buyer Contact: Email"},
+      {"label":"Fax",             "value":"Primary Buyer Contact: Fax"},
+      {"label":"Phone",           "value":"Primary Buyer Contact: Business Name"},
+      {"label":"Mobile Phone",    "value":"Primary Buyer Contact: Mobile Phone"},
+
+    {"label":"subheader",         "value":"Seller Acount"},
+      {"label":"Account Name",    "value":"Seller Account: Account Name"},
+      {"label":"Street Address",  "value":"Seller Account: Billing Street"},
+      {"label":"City",            "value":"Seller Account: Billing City"},
+      {"label":"State",           "value":"Seller Account: Billing State/Province"},
+      {"label":"Zipcode",         "value":"Seller Account: Billing Zip/Postal Code"},
+      {"label":"Country",         "value":"Seller Account: Country"},
+
+    {"label":"subheader",     "value":"Seller Contact (Primary)"},
+      {"label":"Full Name",       "value":"Primary Seller Contact: Full Name"},
+      {"label":"First Name",      "value":"Primary Seller Contact: First Name"},
+      {"label":"Last Name",       "value":"Primary Seller Contact: Last Name"},
+      {"label":"Title",           "value":"Primary Seller Contact: Title"},
+      {"label":"Department",      "value":"Primary Seller Contact: Department"},
+      {"label":"Email",           "value":"Primary Seller Contact: Email"},
+      {"label":"Fax",             "value":"Primary Seller Contact: Fax"},
+      {"label":"Phone",           "value":"Primary Seller Contact: Business Name"},
+      {"label":"Mobile Phone",    "value":"Primary Seller Contact: Mobile Phone"},
+
+    {"label":"subheader",     "value":"Co-Broker Acount"},
+      {"label":"Account Name",    "value":"Co-Broker Account: Account Name"},
+      {"label":"Street Address",  "value":"Co-Broker Account: Billing Street"},
+      {"label":"City",            "value":"Co-Broker Account: Billing City"},
+      {"label":"State",           "value":"Co-Broker Account: Billing State/Province"},
+      {"label":"Zipcode",         "value":"Co-Broker Account: Billing Zip/Postal Code"},
+      {"label":"Country",         "value":"Co-Broker Account: Country"},
+
+    {"label":"subheader",     "value":"Co-Broker Contact (Primary)"},
+      {"label":"Full Name",       "value":"Primary Co-Broker Contact: Full Name"},
+      {"label":"First Name",      "value":"Primary Co-Broker Contact: First Name"},
+      {"label":"Last Name",       "value":"Primary Co-Broker Contact: Last Name"},
+      {"label":"Title",           "value":"Primary Co-Broker Contact: Title"},
+      {"label":"Department",      "value":"Primary Co-Broker Contact: Department"},
+      {"label":"Email",           "value":"Primary Co-Broker Contact: Email"},
+      {"label":"Fax",             "value":"Primary Co-Broker Contact: Fax"},
+      {"label":"Phone",           "value":"Primary Co-Broker Contact: Business Name"},
+      {"label":"Mobile Phone",    "value":"Primary Co-Broker Contact: Mobile Phone"},
+
+  //############################################################################
+  {"label":"header",        "value":"Date"},
+  //############################################################################
+
+    {"label":"subheader",                 "value":"Date Formats"},
+      {"label":"Listing Commencement",    "value":"Listing Commencement|date_format:m/d/Y"},
+      {"label":"Listing Expiration",      "value":"Listing Expiration|date_format:m/d/Y"},
+      {"label":"Create Date",             "value":"Create Date|date_format:m/d/Y"},
+      {"label":"Lease Commencement",      "value":"Lease Commencement|date_format:m/d/Y"},
+      {"label":"Lease Expiration",        "value":"Lease Expiration|date_format:m/d/Y"},
+      {"label":"Option to Renew",         "value":"Option to Renew|date_format:m/d/Y"},
+      {"label":"Option to Terminate",     "value":"Option to Terminate|date_format:m/d/Y"},
+      {"label":"Option to Expand",        "value":"Option to Expand|date_format:m/d/Y"},
+      {"label":"Right of First Refusal",  "value":"Right of First Refusal|date_format:m/d/Y"},
+      {"label":"Due Diligence",           "value":"Due Diligence|date_format:m/d/Y"},
+
+
+];
